aae657c73e836e0a158de66cacf0aee38f5b36a5
IanPrime <98205015+IanPrime@users.noreply.github.com>
2022-02-17 18:12:42 -0600

    Test Push
    idk

--- src/main/java/frc/robot/commands/Index/SingleShot.java
+++ src/main/java/frc/robot/commands/Index/SingleShot.java
@@ -1,47 +1,38 @@
-package frc.robot.commands.conveyor;
+package frc.robot.commands.Index;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Index;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
-
+//plz 
 public class SingleShot extends CommandBase {
 
     private Shooter shooter;
-    private Conveyor conveyor;
+    private Index index;
     private Intake intake;
 
-    public SingleShot(Shooter shooter, Intake intake, Conveyor conveyor){
+    public SingleShot(Shooter shooter, Intake intake, Index index){
         this.shooter = shooter;
-        this.conveyor = conveyor;
+        this.index = index;
         this.intake = intake;
     }
 
     @Override
     public void initialize() {
-        shooter.resetBallsShot();
-        conveyor.enable();
-        conveyor.setConveyorState(Conveyor.ConveyorState.SHOOTING);
+        index.setIndexState(Index.IndexState.DEFAULT);
         intake.setIntakePower(0.5);
-        System.out.println("Single Shot Initialized. Balls Shot = " + shooter.getBallsShot());
     }
 
     @Override
     public void execute() {
-        if(shooter.getBallsShot() != 0) {
-            conveyor.disable();
-            conveyor.setConveyorState(Conveyor.ConveyorState.DEFAULT);
-            conveyor.updatePowerCellCount(shooter.getBallsShot());
-        }
+
     }
 
     @Override
     public void end(boolean interrupted) {
         intake.disbale();
         shooter.disable();
-        conveyor.disable();
-        System.out.println("Single Shot Ended. Balls Shot: " + shooter.getBallsShot());
-        shooter.resetBallsShot();
-        conveyor.setConveyorState(Conveyor.ConveyorState.DEFAULT);
+        index.disable();
+        index.setIndexState(Index.IndexState.DEFAULT);
     }
 }
